ab4af5292723d324662c599c441aa1265d113ece
yujinpan <1192878390@qq.com>
2019-11-17 19:13:40 +0800

    update example

--- examples/App.vue
+++ examples/App.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="app" class="full-height">
-    <h2>vue-highlight</h2>
-    <h4>version: {{ version }}</h4>
+    <h2>Vue-highlight</h2>
+    <h4>Version: {{ version }}</h4>
     <h4>
-      github:
+      Github:
       <a target="_blank" href="https://github.com/yujinpan/vue-highlight"
         >https://github.com/yujinpan/vue-highlight</a
       >
     </h4>
-    <h4>author: yujinpan</h4>
+    <h4>Author: yujinpan</h4>
     <span v-for="item in usage" :key="item">
       <h3>{{ item.title }}</h3>
       <Highlight
@@ -37,22 +37,22 @@ export default {
       version: pak.version,
       usage: [
         {
-          title: 'usage - install:',
+          title: 'Install:',
           codes: [`npm install --save vue-highlight`]
         },
         {
-          title: 'usage - global:',
+          title: 'Usage global:',
           codes: [
             `import Vue from 'vue';\nimport VueHighlight from 'vue-highlight';\nimport 'highlight.js/styles/atom-one-light.css'; // import code style\n\nVue.use(VueHighlight);`,
             `<Highlight :code="code" lang="xml" />`
           ]
         },
         {
-          title: 'usage - local:',
+          title: 'Usage local:',
           codes: [exampleTemplate]
         },
         {
-          title: 'add language pack:',
+          title: 'Add language pack:',
           codes: [
             `import highlight from 'highlight.js/lib/highlight';\nimport java from 'highlight.js/lib/languages/java';\n\nhighlight.registerLanguage('java', java);`,
             `<Highlight :code="javaCode" lang="java" />`
